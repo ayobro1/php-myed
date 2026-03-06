4dd09f2b6ffc240f19a752da0273b265ab3966ff
Jude Kim <75868541+judekim0507@users.noreply.github.com>
2026-03-05 21:59:43 -0800

    fix: vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import './layout.css';
+	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
-	injectAnalytics();
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
